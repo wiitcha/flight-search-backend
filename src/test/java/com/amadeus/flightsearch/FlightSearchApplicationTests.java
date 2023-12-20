fa4b56f0bb6c0e8f372d7e418244af0dd62a3c48
witcha <emir.witcha@gmail.com>
2023-12-20 17:01:06 +0300

    created project

---
+++ src/test/java/com/amadeus/flightsearch/FlightSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amadeus.flightsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
